b2f804cff44295e1881598fb2c0762fb9b30f773
nnndaru <aji.prabandaru@gmail.com>
2025-11-07 10:44:23 +0700

    Update

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Import data.js
-import { profile } from './data.js';
-
-// Populate profile information
 document.addEventListener('DOMContentLoaded', function () {
   // Initialize Lucide icons
   lucide.createIcons();
